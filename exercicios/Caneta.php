cb515a3d8ea8acde0c36267f04bb8977603a794b
Jonathas Oliveira Conceição <jonholiveira18@hotmail.com>
2021-05-26 19:56:33 -0300

    commitei o código com os métodos especiais set, get e construct

--- exercicios/Caneta.php
+++ exercicios/Caneta.php
@@ -1,10 +1,27 @@
 <?php
 class Caneta {
-    public $modelo;
-    public $cor;
+    public $modelo; 
+    private $cor;
     private $ponta;
-    protected $carga;
-    protected $tampada;
+    private $tampada;
+    
+    public function __construct() {
+        $this->cor = 'Azul';
+        $this->tampar();
+    }
+    
+    public function getModelo(){
+       return $this->modelo;
+    }
+    public function setModelo($m){
+        $this->modelo = $m;
+    }
+    public function getPonta(){
+        return $this->ponta;
+    }
+    public function setPonta($p){
+        $this->ponta = $p;
+    }
     
     public function rabiscar(){
         if ($this->tampada == true) {
--- exercicios/index.php
+++ exercicios/index.php
@@ -9,15 +9,8 @@
         <pre>
         <?php
             require_once 'Caneta.php';
-            $c1 = New Caneta;
-            $c1->modelo = 'BIC cristal';
-            $c1->cor = 'Azul';
-            //$c1->ponta = 0.5;
-            //$c1->carga = 99;
-            //$c1->tampada = true;            
-            print_r($c1);            
-            $c1->tampar();
-            $c1->rabiscar();
+            $caneta1 = New Caneta;
+            print_r($caneta1)
         ?>
         </pre>
     </body>
